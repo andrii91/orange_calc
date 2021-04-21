5c294b78ca60866eb0dea378f2228de7bd1ad58e
Andrii <litvin.andriy91@gmail.com>
2021-04-21 19:00:00 +0300

    fix

--- js/app.js
+++ js/app.js
@@ -1,4 +1,42 @@
 $(document).ready(function () {
+  var jsondata, calcData, myData = [],
+    projectData = [];
+
+  function filterProjectInfo($tagProject, $myData) {
+
+    var projectDataESTime = [],
+      projectDataES;
+
+    projectDataES = $myData.filter(function (item) {
+      return item.tag.includes($tagProject);
+    });
+
+
+    for (var i = projectDataES.length - 1; i >= 0; i--) {
+      projectDataESTime.push(projectDataES[i].time_tracked)
+
+    }
+    if (projectDataESTime.length > 0) {
+      projectDataESTime = projectDataESTime.reduce(function (total, amount) {
+        return total + amount;
+      });
+      projectData.push({
+        'project': $tagProject,
+        'time': projectDataESTime
+      })
+
+    } else {
+      projectData.push({
+        'project': $tagProject,
+        'time': 0
+      })
+
+    }
+  }
+
+
+
+
   var rounded = function (number) {
     return +number.toFixed(2);
   }
@@ -13,12 +51,6 @@ $(document).ready(function () {
     if (day < 10) day = "0" + day;
     return day;
   }
-  /*var filter = {
-    user_id: 6849901,
-    date: ,
-  };*/
-  // var timeTrecker = 0;
-  var jsondata, calcData, myData = [];
 
 
   $('#addjson').click(function () {
@@ -44,10 +76,26 @@ $(document).ready(function () {
     }
 
     for (var i = myData.length - 1; i >= 0; i--) {
-
-
-      $('#calc tbody').append('<tr class="' + myData[i].task_id + '"><td class="userID" data-user_id="' + myData[i].user_id + '">' + myData[i].user + '</td> <td class="date_text">' + myData[i].dateText + '</td> <td class="time_tracked">' + myData[i].time_tracked + '</td> <td class="time_hour"><input type="number" class="time_hour-input form-control" value="'+ rounded(myData[i].time_tracked / 3600000) +'" ></td><td>' + myData[i].task_name + '</td> <td>' + myData[i].tag + '</td><td><input width="50px" class="form-control h_summ" type="number" name="' + myData[i].task_id +'" value="0"></td><td class="summ summ_' + myData[i].task_id +'"></td></td><td><button class="btn my-btn" data-id="' + myData[i].task_id + '">обьединить</button></td> </tr>')
+      $('#calc tbody').append('<tr class="' + myData[i].task_id + '"><td class="userID" data-user_id="' + myData[i].user_id + '">' + myData[i].user + '</td> <td class="date_text">' + myData[i].dateText + '</td> <td class="time_tracked">' + myData[i].time_tracked + '</td> <td class="time_hour"><input type="number" class="time_hour-input form-control" value="' + rounded(myData[i].time_tracked / 3600000) + '" ></td><td>' + myData[i].task_name + '</td> <td>' + myData[i].tag + '</td><td><input width="50px" class="form-control h_summ" type="number" name="' + myData[i].task_id + '" value="0"></td><td class="summ summ_' + myData[i].task_id + '"></td></td><td><button class="btn my-btn btn-primary" data-id="' + myData[i].task_id + '">обьединить</button></td> </tr>')
     }
+
+    /*формируем количество часов для разных проектов*/
+    filterProjectInfo('es', myData);
+    filterProjectInfo('orange', myData);
+    filterProjectInfo('es app', myData);
+    filterProjectInfo('rk web', myData);
+    filterProjectInfo('rk app', myData);
+    filterProjectInfo('chrome extension', myData);
+    filterProjectInfo('shopify plugin', myData);
+    filterProjectInfo('salsa', myData);
+    filterProjectInfo('no-tag', myData);
+
+
+    for (var i = projectData.length - 1; i >= 0; i--) {
+      $('#projectTable tbody').append('<tr><td>' + projectData[i].project + '</td><td class="time_hour"><input type="number" class="time_hour-input form-control" value="' + rounded(projectData[i].time / 3600000) + '" ></td><td><input width="50px" class="form-control h_summ" type="number" name="' + projectData[i].project + '" value="0"></td> <td class="summ summ_' + projectData[i].project + '"></td> </tr>')
+    }
+
+
 
 
     $('.my-btn').click(function () {
@@ -71,48 +119,48 @@ $(document).ready(function () {
 
 
       $('.' + $from.data('id') + '').removeClass('summ_complate').hide();
-      
+
       $from.parents('tr').find('.summ').text(rounded((time_tracked_all / 3600000) * $from.parents('tr').find('input.h_summ').val()))
 
       $from.parents('tr').addClass('active-tr summ_complate');
       $from.hide();
     })
 
-     $('input.h_summ[type="number"]').keyup(function(){
-        $(this).parents('tr').find('.summ_'+$(this).attr('name')).text(rounded($(this).parents('tr').find('.time_hour input').val() * $(this).val()))
+    $('input.h_summ[type="number"]').keyup(function () {
+      $(this).parents('tr').find('.summ_' + $(this).attr('name')).text(rounded($(this).parents('tr').find('.time_hour input').val() * $(this).val()))
+      $(this).parents('tr').addClass('summ_complate')
+      // $('.allSumm-item').show();
+      $('.allSumm-item').show();
+
+    })
+
+    $('#user_id input').change(function () {
+      $('#calc td[data-user_id="' + $(this).attr('name') + '"]').parent().find('input.h_summ').val($(this).val())
+
+      $('#calc td[data-user_id="' + $(this).attr('name') + '"]').parent().find('input.h_summ').each(function () {
+        $(this).parents('tr').find('.summ').text(rounded($(this).parents('tr').find('.time_hour input').val() * $(this).val()))
         $(this).parents('tr').addClass('summ_complate')
         // $('.allSumm-item').show();
-        $('.allSumm-item').css('display', 'flex');
-
+        $('.allSumm-item').show();
       })
+    })
 
-       $('#user_id input').change(function(){
-        $('#calc td[data-user_id="'+$(this).attr('name')+'"]').parent().find('input.h_summ').val($(this).val())
+    $('select').change(function () {
+      var filterUserSumm = $('#user').val();
+      if ($('input.h_summ[name="' + filterUserSumm + '"]').val() > 0) {
 
-        $('#calc td[data-user_id="'+$(this).attr('name')+'"]').parent().find('input.h_summ').each(function(){
+        $('#calc td[data-user_id="' + filterUserSumm + '"]').parent().find('input.h_summ').val($('input.h_summ[name="' + filterUserSumm + '"]').val())
+
+        $('#calc td[data-user_id="' + filterUserSumm + '"]').parent().find('input.h_summ').each(function () {
           $(this).parents('tr').find('.summ').text(rounded($(this).parents('tr').find('.time_hour input').val() * $(this).val()))
           $(this).parents('tr').addClass('summ_complate')
-          // $('.allSumm-item').show();
-          $('.allSumm-item').css('display', 'flex');
+
+          $('.allSumm-item').show();
         })
-      })
+      }
+    })
 
-       $('select').change(function(){
-        var filterUserSumm = $('#user').val();
-        if ($('input.h_summ[name="'+filterUserSumm+'"]').val() > 0) {
-
-          $('#calc td[data-user_id="'+filterUserSumm+'"]').parent().find('input.h_summ').val($('input.h_summ[name="'+filterUserSumm+'"]').val())
-
-          $('#calc td[data-user_id="'+filterUserSumm+'"]').parent().find('input.h_summ').each(function(){
-            $(this).parents('tr').find('.summ').text(rounded($(this).parents('tr').find('.time_hour input').val() * $(this).val()))
-            $(this).parents('tr').addClass('summ_complate')
-
-            $('.allSumm-item').css('display', 'flex');
-          })
-        }
-      })
-       
-    $('.time_hour-input').change(function(){
+    $('.time_hour-input').change(function () {
       $(this).attr('value', $(this).val())
       $(this).parents('tr').find('.summ').text(rounded($(this).val() * $(this).parents('tr').find('.h_summ').val()));
     })
@@ -122,9 +170,9 @@ $(document).ready(function () {
 
 
 
-  /*	calcData = calcData.filter(function (item) {
-  	    return item['User ID'] === 6849901;
-  	});*/
+  /*  calcData = calcData.filter(function (item) {
+        return item['User ID'] === 6849901;
+    });*/
 
   /*
     calcData = calcData.filter(function (item) {
@@ -200,6 +248,9 @@ $(document).ready(function () {
 
     }
 
+    taskFilterProject();
+
+
   });
 
 
@@ -248,6 +299,8 @@ $(document).ready(function () {
 
       taskFilter()
     }
+    taskFilterProject();
+
 
   });
 
@@ -298,6 +351,9 @@ $(document).ready(function () {
 
     }
 
+    taskFilterProject();
+
+
   });
 
 
@@ -306,9 +362,15 @@ $(document).ready(function () {
 
     for (var iu = myDataFilter.length - 1; iu >= 0; iu--) {
 
-      // $('#calc tbody').append('<tr class="' + myDataFilter[iu].task_id + '"><td>' + myDataFilter[iu].user + '</td> <td class="date_text">' + myDataFilter[iu].dateText + '</td> <td class="time_tracked">' + myDataFilter[iu].time_tracked + '</td> <td class="time_hour">' + rounded(myDataFilter[iu].time_tracked / 3600000) + '</td><td>' + myDataFilter[iu].task_name + '</td> <td>' + myDataFilter[iu].tag + '</td><td><button class="btn my-btn" data-id="' + myDataFilter[iu].task_id + '">обьединить</button></td> </tr>')
-      $('#calc tbody').append('<tr class="' + myDataFilter[iu].task_id + '"><td class="userID" data-user_id="' + myDataFilter[i].user_id + '">' + myDataFilter[iu].user + '</td> <td class="date_text">' + myDataFilter[iu].dateText + '</td> <td class="time_tracked">' + myDataFilter[iu].time_tracked + '</td> <td class="time_hour"><input type="number" class="time_hour-input form-control" value="' + rounded(myDataFilter[iu].time_tracked / 3600000) + '" ></td><td>' + myDataFilter[iu].task_name + '</td> <td>' + myDataFilter[iu].tag + '</td><td><input width="50px" class="form-control h_summ" type="number" name="' + myDataFilter[iu].task_id +'" value="0"></td><td class="summ summ_' + myDataFilter[iu].task_id +'"></td></td><td><button class="btn my-btn" data-id="' + myDataFilter[iu].task_id + '">обьединить</button></td> </tr>')
+      // $('#calc tbody').append('<tr class="' + myDataFilter[iu].task_id + '"><td>' + myDataFilter[iu].user + '</td> <td class="date_text">' + myDataFilter[iu].dateText + '</td> <td class="time_tracked">' + myDataFilter[iu].time_tracked + '</td> <td class="time_hour">' + rounded(myDataFilter[iu].time_tracked / 3600000) + '</td><td>' + myDataFilter[iu].task_name + '</td> <td>' + myDataFilter[iu].tag + '</td><td><button class="btn my-btn btn-primary" data-id="' + myDataFilter[iu].task_id + '">обьединить</button></td> </tr>')
+      $('#calc tbody').append('<tr class="' + myDataFilter[iu].task_id + '"><td class="userID" data-user_id="' + myDataFilter[i].user_id + '">' + myDataFilter[iu].user + '</td> <td class="date_text">' + myDataFilter[iu].dateText + '</td> <td class="time_tracked">' + myDataFilter[iu].time_tracked + '</td> <td class="time_hour"><input type="number" class="time_hour-input form-control" value="' + rounded(myDataFilter[iu].time_tracked / 3600000) + '" ></td><td>' + myDataFilter[iu].task_name + '</td> <td>' + myDataFilter[iu].tag + '</td><td><input width="50px" class="form-control h_summ" type="number" name="' + myDataFilter[iu].task_id + '" value="0"></td><td class="summ summ_' + myDataFilter[iu].task_id + '"></td></td><td><button class="btn my-btn btn-primary" data-id="' + myDataFilter[iu].task_id + '">обьединить</button></td> </tr>')
     }
+
+    $('.allSumm-item span').each(function(){
+        $(this).text('0');
+    })
+
+
 
 
 
@@ -333,69 +395,100 @@ $(document).ready(function () {
 
 
       $('.' + $from.data('id') + '').removeClass('summ_complate').hide();
-      
+
       $from.parents('tr').find('.summ').text(rounded((time_tracked_all / 3600000) * $from.parents('tr').find('input.h_summ').val()))
 
       $from.parents('tr').addClass('active-tr summ_complate');
       $from.hide();
     })
 
-      $('input.h_summ[type="number"]').keyup(function(){
-        $(this).parents('tr').find('.summ_'+$(this).attr('name')).text(rounded($(this).parents('tr').find('.time_hour input').val() * $(this).val()))
-         $(this).parents('tr').addClass('summ_complate')
-        $('.allSumm-item').css('display', 'flex');
+    $('input.h_summ[type="number"]').keyup(function () {
+      $(this).parents('tr').find('.summ_' + $(this).attr('name')).text(rounded($(this).parents('tr').find('.time_hour input').val() * $(this).val()))
+      $(this).parents('tr').addClass('summ_complate')
+      $('.allSumm-item').show();
 
+    })
+
+
+    $('#user_id input').change(function () {
+      $('#calc td[data-user_id="' + $(this).attr('name') + '"]').parent().find('input.h_summ').val($(this).val())
+
+      $('#calc td[data-user_id="' + $(this).attr('name') + '"]').parent().find('input.h_summ').each(function () {
+        $(this).parents('tr').find('.summ').text(rounded($(this).parents('tr').find('.time_hour input').val() * $(this).val()))
+        $(this).parents('tr').addClass('summ_complate')
+        // $('.allSumm-item').show();
+        $('.allSumm-item').show();
       })
+    })
 
 
-      $('#user_id input').change(function(){
-        $('#calc td[data-user_id="'+$(this).attr('name')+'"]').parent().find('input.h_summ').val($(this).val())
+    $('select').change(function () {
+      var filterUserSumm = $('#user').val();
+      if ($('input.h_summ[name="' + filterUserSumm + '"]').val() > 0) {
 
-        $('#calc td[data-user_id="'+$(this).attr('name')+'"]').parent().find('input.h_summ').each(function(){
+        $('#calc td[data-user_id="' + filterUserSumm + '"]').parent().find('input.h_summ').val($('input.h_summ[name="' + filterUserSumm + '"]').val())
+
+        $('#calc td[data-user_id="' + filterUserSumm + '"]').parent().find('input.h_summ').each(function () {
           $(this).parents('tr').find('.summ').text(rounded($(this).parents('tr').find('.time_hour input').val() * $(this).val()))
           $(this).parents('tr').addClass('summ_complate')
-          // $('.allSumm-item').show();
-          $('.allSumm-item').css('display', 'flex');
+
+          $('.allSumm-item').show();
         })
-      })
+      }
+    })
 
-
-      $('select').change(function(){
-        var filterUserSumm = $('#user').val();
-        if ($('input.h_summ[name="'+filterUserSumm+'"]').val() > 0) {
-
-          $('#calc td[data-user_id="'+filterUserSumm+'"]').parent().find('input.h_summ').val($('input.h_summ[name="'+filterUserSumm+'"]').val())
-
-          $('#calc td[data-user_id="'+filterUserSumm+'"]').parent().find('input.h_summ').each(function(){
-            $(this).parents('tr').find('.summ').text(rounded($(this).parents('tr').find('.time_hour input').val() * $(this).val()))
-            $(this).parents('tr').addClass('summ_complate')
-
-            $('.allSumm-item').css('display', 'flex');
-          })
-        }
-      })
-
-      $('.time_hour-input').change(function(){
-        $(this).attr('value', $(this).val())
-        $(this).parents('tr').find('.summ').text(rounded($(this).val() * $(this).parents('tr').find('.h_summ').val()));
-      })
+    $('.time_hour-input').change(function () {
+      $(this).attr('value', $(this).val())
+      $(this).parents('tr').find('.summ').text(rounded($(this).val() * $(this).parents('tr').find('.h_summ').val()));
+    })
   }
 
+  function taskFilterProject() {
+    $('#projectTable tbody tr').remove();
+    projectData = [];
+    filterProjectInfo('es', myDataFilter);
+    filterProjectInfo('orange', myDataFilter);
+    filterProjectInfo('es app', myDataFilter);
+    filterProjectInfo('rk web', myDataFilter);
+    filterProjectInfo('rk app', myDataFilter);
+    filterProjectInfo('chrome extension', myDataFilter);
+    filterProjectInfo('shopify plugin', myDataFilter);
+    filterProjectInfo('salsa', myDataFilter);
+    filterProjectInfo('no-tag', myDataFilter);
 
-  $('#allSummBtn').click(function(){
+
+    for (var i = projectData.length - 1; i >= 0; i--) {
+      $('#projectTable tbody').append('<tr><td>' + projectData[i].project + '</td><td class="time_hour"><input type="number" class="time_hour-input form-control" value="' + rounded(projectData[i].time / 3600000) + '" ></td><td><input width="50px" class="form-control h_summ" type="number" name="' + projectData[i].project + '" value="0"></td> <td class="summ summ_' + projectData[i].project + '"></td> </tr>')
+    }
+
+
+    $('input.h_summ[type="number"]').keyup(function () {
+      $(this).parents('tr').find('.summ_' + $(this).attr('name')).text(rounded($(this).parents('tr').find('.time_hour input').val() * $(this).val()))
+      $(this).parents('tr').addClass('summ_complate')
+      $('.allSumm-item').show();
+
+    })
+
+    $('.time_hour-input').change(function () {
+      $(this).attr('value', $(this).val())
+      $(this).parents('tr').find('.summ').text(rounded($(this).val() * $(this).parents('tr').find('.h_summ').val()));
+    })
+
+  }
+
+  $('#allSummBtn').click(function () {
     var summ_complate = 0,
-        summ_clock = 0;
-    $('.summ_complate').each(function(){
+      summ_clock = 0;
+    $('#calc .summ_complate').each(function () {
       summ_complate = summ_complate + Number($(this).find('.summ').text())
       summ_clock = summ_clock + Number($(this).find('.time_hour-input').val())
     })
 
-    $('#allSumm').text('$'+rounded(summ_complate));
-    $('#allClock').text(rounded(summ_clock) +" h");
+    $('#allSumm').text('$' + rounded(summ_complate));
+    $('#allClock').text(rounded(summ_clock) + " h");
   })
 
 
 
 
 })
-
